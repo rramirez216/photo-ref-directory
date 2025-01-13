e2c3fe6c90486c74732c42151cb213c03db07db8
Ruben Ramirez <rramirez110911@gmail.com>
2025-01-12 21:40:36 -0800

    lists the files inside a reference directory

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import os
 def main():
     home_dir = os.environ["HOME"]
     home_dir_list = os.listdir(home_dir)
+    ref_photo_dir = None
     for directory in home_dir_list:
         if directory == "Art":
-            print(os.listdir(f"{home_dir}/{'Art'}"))
+            # print(os.listdir(f"{home_dir}/{'Art'}"))
+            ref_photo_dir = os.listdir(f"{home_dir}/{'Art'}")
+    for directory in ref_photo_dir:
+        if directory == "reference photos":
+            print(
+                os.listdir(
+                    f"{home_dir}/Art/reference photos/Ultimate Female Poses Sample Pack"
+                )
+            )
 
 
 main()
