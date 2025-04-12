33308ef3f49532b55917c9a5d9bff6e27b4b09b0
Ruben Ramirez <rramirez110911@gmail.com>
2025-04-11 23:03:31 -0700

    adds logic that first lists the directoryies and then allows the user to choose the directory to use

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ import os
 def main():
     home_dir = os.environ["HOME"]
     home_dir_list = os.listdir(home_dir)
+    list_of_directories_in_home = []
     ref_photo_dir = None
     for directory in home_dir_list:
-        if directory == "Art":
-            # print(os.listdir(f"{home_dir}/{'Art'}"))
-            ref_photo_dir = os.listdir(f"{home_dir}/{'Art'}")
-    for directory in ref_photo_dir:
-        list = []
-        if directory == "reference photos":
-            currentDir = os.listdir(f"{home_dir}/Art/{directory}")
-            for file in currentDir:
-                list.append(currentDir)
-        print(list)
+        list_of_directories_in_home.append(directory)
+    ref_photo_dir = input(
+        f"{list_of_directories_in_home}type in the directory name that contains the reference image files."
+    )
+    print(ref_photo_dir)
+    # if directory == "Art":
+    #     # print(os.listdir(f"{home_dir}/{'Art'}"))
+    #     ref_photo_dir = os.listdir(f"{home_dir}/{'Art'}")
+    # for directory in ref_photo_dir:
+    #     list = []
+    #     if directory == "reference photos":
+    #         currentDir = os.listdir(f"{home_dir}/Art/{directory}")
+    #         for file in currentDir:
+    #             list.append(currentDir)
+    #     print(list)
 
 
 if __name__ == "__main__":
