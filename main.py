119cb6be297ad01cce5a1b1739d39863bee1bf3c
Ruben Ramirez <rramirez110911@gmail.com>
2025-04-12 17:36:23 -0700

    formats the f string inside the input function

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 import os
+from helper_functions import *
 
 
 def main():
     home_dir = os.environ["HOME"]
-    home_dir_list = os.listdir(home_dir)
-    list_of_directories_in_home = []
+    home_dir_list = filter_directories_with_period(os.listdir(home_dir))
+    list_of_directories_in_home = "list of Directories:\n\n"
     ref_photo_dir = None
-    for directory in home_dir_list:
-        list_of_directories_in_home.append(directory)
+    for i in range(len(home_dir_list)):
+        if not (home_dir_list[i].startswith(".")):
+            list_of_directories_in_home += f"{i + 1}.) {home_dir_list[i]}\n"
     ref_photo_dir = input(
-        f"{list_of_directories_in_home}type in the directory name that contains the reference image files."
+        (
+            f"{list_of_directories_in_home}Please type in the directory name "
+            "that contains the reference image files: "
+        )
     )
     print(ref_photo_dir)
-    # if directory == "Art":
-    #     # print(os.listdir(f"{home_dir}/{'Art'}"))
-    #     ref_photo_dir = os.listdir(f"{home_dir}/{'Art'}")
-    # for directory in ref_photo_dir:
-    #     list = []
-    #     if directory == "reference photos":
-    #         currentDir = os.listdir(f"{home_dir}/Art/{directory}")
-    #         for file in currentDir:
-    #             list.append(currentDir)
-    #     print(list)
 
 
 if __name__ == "__main__":
