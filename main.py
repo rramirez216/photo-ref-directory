b8b43893038d6afa21e1e1923e88a90f88ede6cb
Ruben Ramirez <rramirez110911@gmail.com>
2025-04-15 11:18:11 -0700

    imports shelve module and saves directory name to mydata

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import shelve
 import os
 from pathlib import Path
 from helper_functions import *
 
 
 def main():
+    shelf = shelve.open("mydata")
     home_dir = Path.home()
     home_dir_list = filter_directories_with_period(os.listdir(home_dir))
     list_of_directories_in_home = "list of Directories:\n\n"
     ref_photo_dir = None
+
     for i in range(len(home_dir_list)):
         if not (home_dir_list[i].startswith(".")):
             list_of_directories_in_home += f"{i + 1}.) {home_dir_list[i]}\n"
@@ -17,6 +20,10 @@ def main():
             "that contains the reference image files: "
         )
     )
+    shelf["ref_photo_dir"] = ref_photo_dir
+    print(f"shelf file :  {shelf["ref_photo_dir"]}")
+    shelf.close()
+
     if ref_photo_dir in home_dir_list:
         print(ref_photo_dir)
     else:
