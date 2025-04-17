f5fec0a109f189da5847293ebf3a2beb723368b1
Ruben Ramirez <rramirez110911@gmail.com>
2025-04-17 12:52:44 -0700

    adds in if statement that runs the directory naming function

--- main.py
+++ main.py
@@ -5,29 +5,28 @@ from helper_functions import *
 
 
 def main():
-    shelf = shelve.open("mydata")
-    home_dir = Path.home()
-    home_dir_list = filter_directories_with_period(os.listdir(home_dir))
-    list_of_directories_in_home = "list of Directories:\n\n"
-    ref_photo_dir = None
+    shelf = shelve.open("save_file")
+    if not ("ref_photo_dir") in shelf:
+        home_dir = Path.home()
+        home_dir_list = filter_directories_with_period(os.listdir(home_dir))
+        list_of_directories_in_home = "list of Directories:\n\n"
+        ref_photo_dir = None
 
-    for i in range(len(home_dir_list)):
-        if not (home_dir_list[i].startswith(".")):
-            list_of_directories_in_home += f"{i + 1}.) {home_dir_list[i]}\n"
-    ref_photo_dir = input(
-        (
-            f"{list_of_directories_in_home}Please type in the directory name "
-            "that contains the reference image files: "
+        for i in range(len(home_dir_list)):
+            if not (home_dir_list[i].startswith(".")):
+                list_of_directories_in_home += f"- {home_dir_list[i]}\n"
+        ref_photo_dir = input(
+            (
+                f"{list_of_directories_in_home}Please type in the directory "
+                "name that contains the reference\n"
+                "image files or create a new one: \n"
+            )
         )
-    )
-    shelf["ref_photo_dir"] = ref_photo_dir
-    print(f"shelf file :  {shelf["ref_photo_dir"]}")
+        shelf["ref_photo_dir"] = ref_photo_dir
+        print(f"shelf file :  {shelf["ref_photo_dir"]}")
+        if not (ref_photo_dir) in home_dir_list:
+            os.makedirs(f"{home_dir}/{ref_photo_dir}")
     shelf.close()
-
-    if ref_photo_dir in home_dir_list:
-        print(ref_photo_dir)
-    else:
-        os.makedirs(f"{home_dir}/{ref_photo_dir}")
 
 
 if __name__ == "__main__":
