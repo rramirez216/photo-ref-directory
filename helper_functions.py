d02ff93e82be30a68651b6306015af2d16b16071
Ruben Ramirez <rramirez110911@gmail.com>
2025-04-23 15:04:03 -0700

    creates function that prints out the command reference

--- helper_functions.py
+++ helper_functions.py
@@ -1,10 +1,23 @@
-def filter_directories_with_period(arr):
+def filter_directories_with_period(arr, path):
     newArr = []
     for item in arr:
-        if not (item.startswith(".") or item.startswith("~")):
+        item_path = path / item
+        if not (item.startswith(".") or item.startswith("~") or item_path.is_file()):
             newArr.append(item)
     return newArr
 
 
 def create_directory_list():
     return
+
+
+def create_command_list():
+    return (
+        "Usage:\n"
+        "    ref [command]\n\n"
+        "Available Commands:\n"
+        "    add\n"
+        "    reset\n"
+        "    current\n"
+        "    shuffle\n"
+    )
